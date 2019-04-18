af4b815564cbcdfd104ceb7ab2b7c853de4ab971
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-18 10:21:03 +0100

    Refactors note-controller-tests
    - Doubles note list to fully isolate controller tests

--- public/src/note-controller-tests.js
+++ public/src/note-controller-tests.js
@@ -1,30 +1,27 @@
+function NoteDouble(text) {
+  this.text = text
+};
+var note = new NoteDouble("test note")
+
+function NotesListDouble() {};
+NotesListDouble.prototype = {
+  returnList: function() { return [note]}
+};
+var notesList = new NotesListDouble;
+
 function testThingIsNoteControllerObject() {
-  var noteList = new NotesList
-  var noteController = new NoteController(noteList)
-  assert.isTrue(noteController instanceof NoteController)
+  var noteController = new NoteController(notesList);
+  assert.isTrue(noteController instanceof NoteController);
 };
 testThingIsNoteControllerObject();
 
 function testControllerRendersNotesList() {
-  
-  function NoteDouble(text) {
-    this.text = text
-  };
-  var note = new NoteDouble("test note")
-
-  function NotesListDouble() {};
-  NotesListDouble.prototype = {
-    returnList: function() { return [note]}
-  }
- 
-  function element() {
+  function elementDouble() {
     this.innerHtml = ""
   }
  
-  var notesList = new NotesListDouble
-
   var noteController = new NoteController(notesList)
-  var newHtml = noteController.renderHtml(element)
+  var newHtml = noteController.renderHtml(elementDouble)
   assert.isTrue(newHtml === "<ul><li>test note</li></ul>")
 };
 testControllerRendersNotesList();
