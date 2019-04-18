20b29a826707be723a6abb7c0d21dc1560780901
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-18 10:04:42 +0100

    Refactors to isolate notes-list-view-tests
    - Adds NoteDouble and NoteListDouble so that NoteListView can be tested in isolation

--- public/src/notes-list-view-tests.js
+++ public/src/notes-list-view-tests.js
@@ -1,52 +1,57 @@
-// double NotesList in this test file
+function NoteDouble(text) {
+  this.text = text
+}
+
+function NotesListDouble() {
+  this.listArray = []
+}
+NotesListDouble.prototype = {
+  add: function(note) {
+    this.listArray.push(note)
+  },
+  returnList: function() {
+    return this.listArray
+  }
+}
+
+var note1 = new NoteDouble('Buy Eggs');
+var note2 = new NoteDouble('Bacon');
+var notesList = new NotesListDouble;
 
 function testViewModelInstantiation () {
-  function NotesListDouble() {
-    this.listArray = []
-  }
-  NotesListDouble.prototype = {
-    add: function(text) {
-      this.listArray.push(text)
-    },
-    returnList: function() {
-      return this.listArray
-    }
-  }
-  
-  // double added above, now use it in the test below
-  var notesList = new NotesList();
-  notesList.add('Buy Eggs');
-  notesList.add('Bacon');
+  notesList.add(note1);
+  notesList.add(note2);
   var notesListView = new NotesListView(notesList);
   assert.isTrue(notesListView.returnNotesList().listArray[0].text === 'Buy Eggs');
 }
 
 function testViewModelReturnsHtmlForANote () {
-  var notesList = new NotesList();
-  notesList.add('Buy Eggs');
+  var notesList = new NotesListDouble;
+  notesList.add(note1);
   var notesListView = new NotesListView(notesList);
   assert.isTrue(notesListView.returnHtml() === "<ul><li>Buy Eggs</li></ul>");
 }
 
 function testViewModelReturnsHtmlForSeveralNotes () {
-  var notesList = new NotesList();
-  notesList.add('Buy Eggs');
-  notesList.add('Bacon');
+  var notesList = new NotesListDouble;
+  notesList.add(note1);
+  notesList.add(note2);
   var notesListView = new NotesListView(notesList);
   assert.isTrue(notesListView.returnHtml() === "<ul><li>Buy Eggs</li><li>Bacon</li></ul>");
 }
 
 function testViewModelReturnsHtmlNoNotes () {
-  var notesList = new NotesList();
+  var notesList = new NotesListDouble;
   var notesListView = new NotesListView(notesList);
   assert.isTrue(notesListView.returnHtml() === "<ul></ul>");
 }
 
 function testsNotesOnlyShowFirst20Chars() {
-  var notesList = new NotesList
-  notesList.add("Concatenate to twenty characters only")
-  var notesListView = new NotesListView(notesList)
-  assert.isTrue(notesListView.returnHtml() === "<ul><li>Concatenate to twent</li></ul>")
+  var notesList = new NotesListDouble;
+  note3 = new NoteDouble("Concatenate to twenty characters only");
+  notesList.add(note3);
+  var notesListView = new NotesListView(notesList);
+  assert.isTrue(notesListView.returnHtml() === "<ul><li>Concatenate to twent</li></ul>");
 }
 
 
